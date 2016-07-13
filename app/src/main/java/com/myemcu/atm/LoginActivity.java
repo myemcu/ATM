3a5d82b931fad29fa23955433f5de79da156e449
myemcu <1142497680@qq.com>
2016-07-13 11:10:17 +0800

    V2，存储与读取登陆帐号。

--- app/src/main/java/com/myemcu/atm/LoginActivity.java
+++ app/src/main/java/com/myemcu/atm/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.myemcu.atm;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,10 +10,16 @@ import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText UserId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        UserId = (EditText) findViewById(R.id.user);
+        SharedPreferences setting = getSharedPreferences("atm", MODE_PRIVATE);
+        UserId.setText(setting.getString(getString(R.string.userid_write), ""));
     }
 
     //"登陆"事件处理(根据帐号密码来登陆)
@@ -28,6 +35,12 @@ public class LoginActivity extends AppCompatActivity {
 
         // 校验帐号密码
         if(uid.equals("老婆") && pwd.equals("520025")) { // 一个预设的帐号，密码
+
+            //帐号存储(密码不存储)
+            SharedPreferences setting = getSharedPreferences("atm", MODE_PRIVATE);
+            setting.edit().putString(getString(R.string.userid_write), uid).commit();
+
+            //消息提示
             Toast.makeText(this, "登陆到主界面", Toast.LENGTH_SHORT).show();
 
             //将"帐号"与"密码"放入Intent对象中
